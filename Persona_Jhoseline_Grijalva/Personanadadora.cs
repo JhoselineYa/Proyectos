1e7efe44bb51f5d2fb2fc825a237e9082d735fc8
JhoselineYa <iviyokatss036@gmail.com>
2025-02-20 20:50:53 -0600

    Add files via upload

---
+++ Persona_Jhoseline_Grijalva/Personanadadora.cs
@@ -0,0 +1,23 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Persona_Jhoseline_Grijalva
+{
+    public class Personanadadora : Persona
+    {
+        public Personanadadora(string ojos, string altura, string peso, string sexo) : base(ojos, altura, peso, sexo)
+        {
+        }
+
+        public void Nadar()
+        {
+            Console.WriteLine("Nadando para volver al peso ideal");
+            this.Peso = "85";
+        }
+
+    }
+}
---
+++ Persona_Jhoseline_Grijalva/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Persona_Jhoseline_Grijalva
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Persona Personal = new Persona("Verdes", "187", "85", "V");
+            Console.WriteLine("Descripción de la persona: ");
+            Console.WriteLine("Ojos:" + Personal.Ojos);
+            Console.WriteLine("Altura:" + Personal.Altura);
+            Console.WriteLine("Peso:" + Personal.Peso);
+            Console.WriteLine("Sexo:" + Personal.Sexo);
+
+            Personal.Hablar("Me llamo Borja");
+            Console.WriteLine("Color de mis ojos:" + Personal.Ojos);
+            Personal.correr(1000);
+            Console.WriteLine("Mi peso actual es:" + Personal.Peso);
+
+            Personanadadora Persona1 = new Personanadadora("Verdes", "187", "85", "V");
+            Console.WriteLine("Descripción de la persona: ");
+            Console.WriteLine("Ojos:" + Personal.Ojos);
+            Console.WriteLine("Altura:" + Personal.Altura);
+            Console.WriteLine("Peso:" + Personal.Peso);
+            Console.WriteLine("Sexo:" + Personal.Sexo);
+
+            Personal.Hablar("Me llamo Borja");
+            Console.WriteLine("Color de mis ojos:" + Personal.Ojos);
+            Personal.correr(1000);
+            Console.WriteLine("Mi peso actual es:" + Personal.Peso);
+            Persona1.Nadar();
+            Console.WriteLine("Tras nadar peso:" + Personal.Peso);
+            Console.ReadKey();
+
+        }
+    }
+}
